5d00f885ac50144ef7ee2824ae8aab14543a3880
siva6 <hohoonlee@gmail.com>
2025-04-25 15:39:36 +0900

    fix: update comments for exponentiation notation in ex1.10.js

--- ch01/ex1.10.js
+++ ch01/ex1.10.js
@@ -12,8 +12,8 @@ console.log(A(2, 4));
 console.log(A(3, 3));
 
 const f = n => A(0, n);		// 2 * n
-const g = n => A(1, n);		// 2 pow n
-const h = n => A(2, n);		// (2 pow n) pow n
+const g = n => A(1, n);		// 2 ^ n
+const h = n => A(2, n);		// (2 ^ n) ^ n
 const k = n => 5 * n * n;
 
 console.log(g(4));
